0bfd7ca83d262a3dfb1a87d05a5d3aa29cd5cdcd
mitocon <136164954+mitocon@users.noreply.github.com>
2023-08-15 21:48:08 +0900

    Recursion 34のリファクタリング

--- recursion/34.php
+++ recursion/34.php
@@ -11,18 +11,19 @@
  */
 
 function isValidPassword(string $password): bool{
+    // preg_matchの戻り値は0か1のため、0か1かの比較演算子は不要
+
     // 半角スペースがあればfalse
-    if(preg_match('/ /', $password) == 1) return false;
+    if(preg_match('/ /', $password)) return false;
     // 小文字を含む
-    if(!(preg_match('/[a-z]/', $password) > 0)) return false;
+    if(!preg_match('/[a-z]/', $password)) return false;
     // 大文字を含む
-    if(!(preg_match('/[A-Z]/', $password) > 0)) return false;
+    if(!preg_match('/[A-Z]/', $password)) return false;
     // 数字を含む
-    if(!(preg_match('/\d/', $password) > 0)) return false;
+    if(!preg_match('/\d/', $password)) return false;
     // 文字列が6文字以上である
-    if(!(strlen($password) >= 6)) return false;
+    if((strlen($password) < 6)) return false;
     // 記号は含まない
-    if(preg_match('/[^a-zA-Z0-9]/', $password) == 1) return false;
+    if(preg_match('/[^a-zA-Z0-9]/', $password)) return false;
     return true;
 }
-
