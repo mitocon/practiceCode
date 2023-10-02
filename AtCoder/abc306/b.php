4362897c105968b1a58baac6f47f08ce525cdf8a
mitocon <mitocon496@gmail.com>
2023-10-02 21:47:53 +0900

    タイポ修正

--- AtCoder/abc306/b.php
+++ AtCoder/abc306/b.php
@@ -2,5 +2,5 @@
 // https://atcoder.jp/contests/abc306/tasks/abc306_b
 
 $aList = fgets(STDIN);
-$binaryNumber=strrev(str_replace(" ","",$a_list))
+$binaryNumber = strrev(str_replace(" ", "", $a_list));
 echo gmp_init($binaryNumber, 2);
